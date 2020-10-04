d237d99f88e35167a54db338aed8f6201e7fe02e
ilyamarvin <ilya.sereda2002@mail.ru>
2020-10-04 13:04:20 +0300

    Winner2048Player don't work yet.

---
+++ src/ru/mirea/additionaltask/game2048/players/Winner2048Player.java
@@ -0,0 +1,33 @@
+package ru.mirea.additionaltask.game2048.players;
+
+import ru.mirea.additionaltask.game2048.Game2048Field;
+import ru.mirea.additionaltask.game2048.Game2048Player;
+
+import java.util.Random;
+
+public class Winner2048Player extends Game2048Player {
+
+    private Random random = new Random();
+
+    public Winner2048Player(Game2048Field game2048Field) {
+        super(game2048Field);
+    }
+
+    @Override
+    protected void step() {
+        switch (random.nextInt(5)) {
+            case 0:
+                game2048Field.moveRight();
+                break;
+            case 1:
+                game2048Field.moveDown();
+                break;
+            case 2:
+                game2048Field.moveLeft();
+                break;
+            case 3:
+                game2048Field.moveUp();
+                break;
+        }
+    }
+}
