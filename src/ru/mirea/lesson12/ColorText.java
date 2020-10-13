5b6c5893576af2a63b36146d05edf70adf5e5cb4
ilyamarvin <ilya.sereda2002@mail.ru>
2020-10-13 19:16:03 +0300

    Did 12 lesson. Coloring the text is done using the random function.

---
+++ src/ru/mirea/lesson12/ColorText.java
@@ -0,0 +1,26 @@
+package ru.mirea.lesson12;
+
+public enum ColorText {
+    RED("\u001B[31m"),
+    YELLOW("\u001B[33m"),
+    BLUE ("\u001B[34m"),
+    WHITE( "\u001B[37m"),
+    GREEN( " \u001B[32m");
+
+    private String colorType;
+
+
+    ColorText(String colorType) {
+        this.colorType = colorType;
+    }
+
+    public String getColorType() {
+        return colorType;
+    }
+
+    public void setColorType(String colorType) {
+        this.colorType = colorType;
+    }
+}
+
+
