cfd8603e6b900026c2481133869086b7c18c92bd
ilyamarvin <ilya.sereda2002@mail.ru>
2020-10-20 03:03:52 +0300

    Works again, but not according to the terms of reference.

--- src/ru/mirea/task14/ChangeCharsByReplace.java
+++ src/ru/mirea/task14/ChangeCharsByReplace.java
@@ -1,6 +1,8 @@
 package ru.mirea.task14;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChangeCharsByReplace {
 
@@ -8,13 +10,14 @@ public class ChangeCharsByReplace {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         String[] parts;
-        int N;
+        char[] ch;
+        int N, j;
         String rule, word;
 
         System.out.println("Введите количество правил: ");
         N = scanner.nextInt();
 
-        // функция по обходу пропажи первого элемента при вызове nextLine() после nextInt()
+        // функция по обходу исчезновения первого элемента при вызове nextLine() после nextInt()
         scanner.nextLine();
 
         System.out.println("Введите правила замены элементов (через пробел): ");
@@ -27,18 +30,53 @@ public class ChangeCharsByReplace {
         System.out.println("Введите строку, к которой хотите применить правила изменения: ");
         word = scanner.nextLine();
 
-        for (int i = 0; i < N; i++) {
+        // Замена переменных до последнего совпадения
+        /*for (int i = 0; i < N; i++) {
             rule = list.get(i);
             parts = rule.split(" ");
-            String part1 = parts[0];
-            String part2 = parts[1];
-
-            // Замена переменных до последнего совпадения
+            part1 = parts[0];
+            part2 = parts[1];
             word = word.replaceAll(part1, part2);
             System.out.println(word);
-        }
+        }*/
 
+        // Вторая замена переменных до последнего совпадения
+        /*for (int i = 0; i < N; i++) {
+            word = word.replace(list.get(i).split(" ")[0], list.get(i).split(" ")[1]);
+            System.out.println(list.get(i).split(" ")[0] + " " +  list.get(i).split(" ")[1]);
+            System.out.println(word);
+        }*/
+
+        // Третья замена переменных до последнего совпадения 🤯
+        // Помогите... Не получается сделать по ТЗ 😤
+        /*for (int i = 0; i < N; i++) {
+            Pattern p = Pattern.compile(list.get(i).split(" ")[0]);
+            Matcher m = p.matcher(word);
+            word = m.replaceAll(list.get(i).split(" ")[1]);
+        }
+        System.out.println(word);*/
+
+
+        // Jesus, HELP ME (time 3:02 ночи, зачем я это делаю...)
+        // Оно опять работает не по ТЗ
+        for (int i = 0; i < N; i++) {
+            ch = new char[list.get(i).split(" ")[0].length()];
+
+            for (int k = 0; k < word.length(); k++) {
+                if (list.get(i).split(" ")[0].length() + k < word.length()) {
+                    word.getChars(k, list.get(i).split(" ")[0].length() + k, ch, 0);
+                    System.out.println(ch);
+                    System.out.println(list.get(i).split(" ")[0].toCharArray());
+
+                    if (Arrays.equals(ch, list.get(i).split(" ")[0].toCharArray())) {
+                        System.out.println(i);
+                        word = word.replace(list.get(i).split(" ")[0], list.get(i).split(" ")[1]);
+                        System.out.println(word);
+                        i = 0;
+                    }
+                }
+            }
+        }
         System.out.println(word);
     }
 }
-
